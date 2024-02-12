44ed1c189970b29d4d6d9e17012ae77fbb9af579
Essa_work <essa4a@gmail.com>
2024-02-12 14:16:57 +0300

    register models to admin.py

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
-
+from .models import BasicInfo, Skill, Course, Education, Experience, Fact, Service, Project
 # Register your models here.
+admin.site.register(BasicInfo)
+admin.site.register(Skill)
+admin.site.register(Course)
+admin.site.register(Education)
+admin.site.register(Experience)
+admin.site.register(Fact)
+admin.site.register(Service)
+admin.site.register(Project)
