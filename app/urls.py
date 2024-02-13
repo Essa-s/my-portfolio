8f7ac9de65e46b324680ee693cbddd46c453bc03
Essa_work <essa4a@gmail.com>
2024-02-13 11:52:11 +0300

    add serializer & basic view api

--- app/urls.py
+++ app/urls.py
@@ -5,6 +5,6 @@ from . import views
 app_name = 'app'
 urlpatterns = [
     path("", views.home, name="app"),
-
-
+    path('basic=info-view/', views.BasicInfoListView.as_view(), name='basic-info-view'),
+    path('basic-info-create/', views.BasicInfoCreateView.as_view(), name='basic-info-create'),
 ]
--- app/views.py
+++ app/views.py
@@ -1,5 +1,18 @@
 from django.shortcuts import render
-
+from rest_framework import generics, permissions
+from .models import BasicInfo
+from .serializers import BasicInfoSerializer
 # Create your views here.
 def home(request):
     return render(request, 'index.html', {})
+
+class BasicInfoCreateView(generics.CreateAPIView):
+    queryset = BasicInfo.objects.all()
+    serializer_class = BasicInfoSerializer
+    permission_classes = [permissions.IsAuthenticated]
+    http_method_names = ['post']
+
+class BasicInfoListView(generics.ListAPIView):
+    queryset = BasicInfo.objects.all()
+    serializer_class = BasicInfoSerializer
+    http_method_names = ['get']
